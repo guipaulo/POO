5fca6a3a501db768ae014b1304ed898af13d0f60
pauloguilherme <pauloguilherme_cn@outlook.com>
2022-03-22 11:04:53 -0300

    Create main.cpp

---
+++ CompararCarros/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  char resp;
+  char entrada;
+  int AnoCarro;
+  float Velocidade, VelocidadeTotal;
+  int contador = 0;
+
+  while (true) {
+    cout << "Teste\n";
+    cin >> resp;
+    cin >> AnoCarro;
+    cin >> Velocidade;
+    cin >> resp;
+    contador += 1;
+  }
+}
